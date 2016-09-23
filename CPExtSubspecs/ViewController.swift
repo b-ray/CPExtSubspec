b2a2e248dfb87019f51fc4ee4ce420b1256f073c
Stefan Puehringer <stefan.puehringer@runtastic.com>
2016-09-23 12:39:58 +0200

    Initial Commit

---
+++ CPExtSubspecs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPExtSubspecs
+//
+//  Created by Stefan Puehringer on 23/09/16.
+//  Copyright © 2016 Stefan Puehringer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
